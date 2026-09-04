c19e798d7fe93f7bbe0dd71df3f30d757f9fe4ab
Reir17 <irpanyy87@gmail.com>
2026-09-05 01:48:41 +0700

    fix: update next.config.ts and remove --no-lint flag

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  // Abaikan error TypeScript saat build di Vercel (opsional)
   typescript: {
     ignoreBuildErrors: true,
   },
